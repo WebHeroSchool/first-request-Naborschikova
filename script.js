28b65b9e556efa0d215334cdbfb8a3f359a08452
lisaredfox <79074708+lisaredfox@users.noreply.github.com>
2022-06-27 03:13:36 +0500

    Add files via upload
    ДЗ выполнено, проект опубликован

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 
-fetch('https://api.github.com/users/lisaredfox')
+let information = setTimeout(function loadScript() {fetch('https://api.github.com/users/lisaredfox')
     .then (res => res.json())
     .then (json => {
         let photo = json.avatar_url;
-        let name = json.name;
-        let description = json.bio;
-        let body = document.getElementsByTagName('body');
         let picture = document.createElement('img');
         document.body.append(picture);
         picture.src = photo;
 
         let wrapper = document.createElement('p');
         document.body.append(wrapper);
+        
+        let name = json.name;
         let userName = document.createElement('a');
         userName.href = json.html_url;
         wrapper.append(userName);
@@ -19,9 +18,10 @@ fetch('https://api.github.com/users/lisaredfox')
             userName.innerHTML = 'Elizaveta Naborschikova'}
             else if (name == undefined){
                 userName.innerHTML = 'Elizaveta Naborschikova'}
-            else {userName.innerHTML = name};            
+                else {userName.innerHTML = name};            
 
-            let userDescription = document.createElement('p');
+        let description = json.bio;
+        let userDescription = document.createElement('p');
         document.body.append(userDescription);
          if (description == null){
             userDescription.innerHTML = 'ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ НЕДОСТУПНА'}
@@ -29,3 +29,10 @@ fetch('https://api.github.com/users/lisaredfox')
                 userDescription.innerHTML = 'ИНФОРМАЦИЯ О ПОЛЬЗОВАТЕЛЕ НЕДОСТУПНА'}
             else {userDescription.innerHTML = description};
     })
+    .finally(() => {let preload = document.querySelector('.spinner');
+                    preload.classList.add('hide')})}, 3000)
+
+    let today = new Promise(() => {setTimeout(() => console.log(new Date), 2000)});
+
+  Promise.all([information, today]).then(() => [information, today])
+
